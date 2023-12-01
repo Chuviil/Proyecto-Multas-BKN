c4bdc77b8278b2ce106cff6a15a8258cfb011535
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-30 23:18:43 -0500

    added environment variables for database connection

--- src/config.js
+++ src/config.js
@@ -2,4 +2,8 @@ import "dotenv/config";
 
 export default {
     PORT: process.env.PORT,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
 }
