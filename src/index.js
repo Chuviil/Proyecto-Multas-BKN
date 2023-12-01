f43177d4ef8061562c01c7aff4d58dc2272b5380
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-01 00:15:41 -0500

    modified index.js to import 3 models

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import app from "./app.js";
 import config from "./config.js"
 import {sequelize} from "./database/database.js";
 
+import "./models/Profesor.js"
+import "./models/Ayudante.js"
+import "./models/Multa.js"
+
 const main = async () => {
     try {
-        await sequelize.authenticate();
-        console.log("Connection to SQL Server has been established successfully.");
+        await sequelize.sync({alter: true});
         app.listen(config.PORT, () => {
-            console.log(`Server is running on port ${config.PORT}`);
+            console.log("Server is running on port", config.PORT);
         });
     } catch (e) {
         console.error(e);
