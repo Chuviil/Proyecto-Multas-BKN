2e44e76d125cca954500829003885c19409d8666
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-01 22:22:11 -0500

    added PUT update, DELETE delete, GET by id for Ayudante

--- src/controllers/ayudantes.controller.js
+++ src/controllers/ayudantes.controller.js
@@ -1,5 +1,19 @@
 import {Ayudante} from "../models/Ayudante.js";
 
+export const crearAyudante = async (req, res) => {
+    try {
+        const {nombre, carrera, contrasenia} = req.body;
+        const nuevoAyudante = await Ayudante.create({
+            Nombre: nombre,
+            Carrera: carrera,
+            Contrasenia: contrasenia
+        })
+        res.json(nuevoAyudante);
+    } catch (e) {
+        res.status(500).json({message: e.message});
+    }
+}
+
 export const obtenerAyudantes = async (req, res) => {
     try {
         const ayudantes = await Ayudante.findAll();
@@ -9,16 +23,45 @@ export const obtenerAyudantes = async (req, res) => {
     }
 }
 
-export const crearAyudante = async (req, res) => {
-    const {nombre, carrera, contrasenia} = req.body;
-
+export const obtenerAyudante = async (req, res) => {
     try {
-        const nuevoAyudante = await Ayudante.create({
-            Nombre: nombre,
-            Carrera: carrera,
-            Contrasenia: contrasenia
-        })
-        res.json(nuevoAyudante);
+        const {idBanner} = req.params;
+        const ayudante = await Ayudante.findByPk(idBanner);
+        if (!ayudante) return res.status(404).json({message: "Ayudante no encontrado"});
+        res.json(ayudante);
+    } catch (e) {
+        res.status(500).json({message: e.message});
+    }
+}
+
+export const actualizarAyudante = async (req, res) => {
+    try {
+        const {idBanner} = req.params;
+        const {nombre, carrera} = req.body;
+
+        const ayudante = await Ayudante.findByPk(idBanner);
+
+        ayudante.Nombre = nombre;
+        ayudante.Carrera = carrera;
+
+        await ayudante.save();
+
+        res.json(ayudante);
+    } catch (e) {
+        res.status(500).json({message: e.message});
+    }
+}
+
+export const eliminarAyudante = async (req, res) => {
+    try {
+        const {idBanner} = req.params;
+        await Ayudante.destroy({
+            where: {
+                IdBanner: idBanner
+            }
+        });
+
+        res.sendStatus(204);
     } catch (e) {
         res.status(500).json({message: e.message});
     }
--- src/routes/ayudantes.routes.js
+++ src/routes/ayudantes.routes.js
@@ -1,12 +1,17 @@
 import {Router} from "express";
-import {crearAyudante, obtenerAyudantes} from "../controllers/ayudantes.controller.js";
+import {
+    actualizarAyudante,
+    crearAyudante,
+    eliminarAyudante, obtenerAyudante,
+    obtenerAyudantes
+} from "../controllers/ayudantes.controller.js";
 
 const router = Router();
 
 router.post("/", crearAyudante);
 router.get("/", obtenerAyudantes);
-router.get("/:idBanner");
-router.put("/:idBanner");
-router.delete("/:idBanner");
+router.get("/:idBanner", obtenerAyudante);
+router.put("/:idBanner", actualizarAyudante);
+router.delete("/:idBanner", eliminarAyudante);
 
 export default router;
