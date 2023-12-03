29b53e756f5adde8cb5543926be1ebdd09936929
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-02 21:47:04 -0500

    documented Profesor schema for swagger

--- src/models/Profesor.js
+++ src/models/Profesor.js
@@ -1,6 +1,23 @@
 import {sequelize} from "../database/database.js";
 import {DataTypes} from "sequelize";
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *   Profesor:
+ *    type: object
+ *    properties:
+ *     idBanner:
+ *      type: string
+ *      example: A00909988
+ *     nombre:
+ *      type: string
+ *      example: Sebastian Quito
+ *     contrasenia:
+ *      type: string
+ *      example: 12345@
+ */
 export const Profesor = sequelize.define("Profesores", {
     IdBanner: {
         type: DataTypes.INTEGER,
