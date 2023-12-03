29a9addfede3efee0a883c0eb633165047bb47ac
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-02 19:09:23 -0500

    created basic setup for swagger ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import app from "./app.js";
 import config from "./config.js"
 import {sequelize} from "./database/database.js";
+import {swaggerDocs} from "./swagger/swagger.js";
 
 const main = async () => {
     try {
         await sequelize.sync({force: true});
         app.listen(config.PORT, () => {
+            swaggerDocs(app, config.PORT);
             console.log("Server is running on port", config.PORT);
         });
     } catch (e) {
