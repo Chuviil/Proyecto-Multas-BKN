376acaee1ecd690a6799403ae98e074a7fca997b
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-02 19:10:18 -0500

    documented Multa schema and routes

--- src/routes/multas.routes.js
+++ src/routes/multas.routes.js
@@ -9,10 +9,86 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ *   /api/Multa:
+ *     post:
+ *       tags:
+ *         - Multas
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Multa'
+ *       responses:
+ *         "200":
+ *           description: "OK"
+ */
 router.post("/", crearMulta);
+
+/**
+ * @swagger
+ *   /api/Multa:
+ *     get:
+ *       tags:
+ *         - Multas
+ *       responses:
+ *         "200":
+ *           description: "OK"
+ */
 router.get("/", obtenerMultas);
+
+/**
+ * @swagger
+ *   /api/Multa/{multaId}:
+ *     get:
+ *       tags:
+ *         - Multas
+ *       parameters:
+ *       - name: "multaId"
+ *         in: "path"
+ *         required: true
+ *       responses:
+ *         "200":
+ *           description: "OK"
+ */
 router.get("/:multaId", obtenerMulta);
+
+/**
+ * @swagger
+ *   /api/Multa/{multaId}:
+ *     put:
+ *       tags:
+ *         - Multas
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Multa'
+ *       parameters:
+ *       - name: "multaId"
+ *         in: "path"
+ *         required: true
+ *       responses:
+ *         "200":
+ *           description: "OK"
+ */
 router.put("/:multaId", actualizarMulta);
+
+/**
+ * @swagger
+ *   /api/Multa/{multaId}:
+ *     delete:
+ *       tags:
+ *         - Multas
+ *       parameters:
+ *       - name: "multaId"
+ *         in: "path"
+ *         required: true
+ *       responses:
+ *         "200":
+ *           description: "OK"
+ */
 router.delete("/:multaId", eliminarMulta);
 
 export default router;
