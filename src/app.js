e34915adc1bce27d82309ba514b5ce68e5d31f73
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-04 17:21:53 -0500

    added morgan to debug all visited routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from 'express';
 import ayudantesRoutes from "./routes/ayudantes.routes.js";
 import multasRoutes from "./routes/multas.routes.js";
 import profesoresRoutes from "./routes/profesores.routes.js";
+import morgan from "morgan";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(morgan("dev"));
 
 // Routes
 app.use("/api/Ayudante", ayudantesRoutes);
